5bab97cac5f3de979e63714b204a26971a04c1dc
bradkcrist <bradkcrist08@gmail.com>
2024-03-07 05:21:42 -0700

    Created a main-view, movie-card and movie-view file and component in each file eporting and importing them all with each other

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+import { MovieCard } from '../movie-card/movie-card';
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: 'The Hobbit: An Unexpected Journey',
+      description:
+        'A reluctant hobbit named Bilbo Baggins is swept into a quest by the wizard Gandalf. Joining a company of dwarves led by Thorin Oakenshield, Bilbo embarks on a perilous journey to reclaim the Lonely Mountain and its treasure from the fearsome dragon Smaug.',
+      genre:
+        'Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.',
+      director: 'Peter Jackson',
+      bio: 'New Zealander director and screenwriter',
+      birth: '10-31-1961',
+    },
+    {
+      id: 2,
+      title: 'The Hobbit: The Desolation of Smaug',
+      description:
+        'Bilbo Baggins navigates perilous landscapes and encounters dangerous creatures as he joins a quest to reclaim treasure guarded by the dragon Smaug. Unraveling secrets and testing alliances, the journey becomes a thrilling adventure where friendships are forged and courage is tested in the heart of Middle-earth.',
+      genre:
+        'Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.',
+      director: 'Peter Jackson',
+      bio: 'New Zealander director and screenwriter',
+      birth: '10-31-1961',
+    },
+    {
+      id: 3,
+      title: 'The Hobbit: The Battle of The Five Armies',
+      description:
+        "Middle-earth is thrown into a spectacular war as the Smaug's wrath unleashes chaos. Heroes and races clash for control of the Lonely Mountain, leading to a climactic battle that will determine the fate of the realm. Loyalties are tested, sacrifices made, and destinies fulfilled in this epic conclusion to Bilbo Baggins's extraordinary journey",
+      genre:
+        'Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.',
+      director: 'Peter Jackson',
+      bio: 'New Zealander director and screenwriter',
+      birth: '10-31-1961',
+    },
+  ]);
+
+  if (selectedMovie) {
+    return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
+  }
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (movies.length === 0) {
+    return <div>The list is empty</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => {
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />;
+      })}
+    </div>
+  );
+};
---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,31 @@
+export const MovieView = ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <div>
+        <span>Bio: </span>
+        <span>{movie.bio}</span>
+      </div>
+      <div>
+        <span>Birth: </span>
+        <span>{movie.birth}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
