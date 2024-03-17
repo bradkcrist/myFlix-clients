ecd2af0eb7475d556d1bc99303514fc76be7176a
bradkcrist <bradkcrist08@gmail.com>
2024-03-17 04:42:53 -0600

    Added an image to my main-view and a <img tag to my movie-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,6 +7,7 @@ export const MainView = () => {
     {
       id: 1,
       title: 'The Hobbit: An Unexpected Journey',
+      image: 'https://upload.wikimedia.org/wikipedia/en/b/b3/The_Hobbit-_An_Unexpected_Journey.jpeg',
       description:
         'A reluctant hobbit named Bilbo Baggins is swept into a quest by the wizard Gandalf. Joining a company of dwarves led by Thorin Oakenshield, Bilbo embarks on a perilous journey to reclaim the Lonely Mountain and its treasure from the fearsome dragon Smaug.',
       genre:
@@ -18,6 +19,7 @@ export const MainView = () => {
     {
       id: 2,
       title: 'The Hobbit: The Desolation of Smaug',
+      image: 'https://m.media-amazon.com/images/M/MV5BMzU0NDY0NDEzNV5BMl5BanBnXkFtZTgwOTIxNDU1MDE@._V1_.jpg',
       description:
         'Bilbo Baggins navigates perilous landscapes and encounters dangerous creatures as he joins a quest to reclaim treasure guarded by the dragon Smaug. Unraveling secrets and testing alliances, the journey becomes a thrilling adventure where friendships are forged and courage is tested in the heart of Middle-earth.',
       genre:
@@ -29,6 +31,7 @@ export const MainView = () => {
     {
       id: 3,
       title: 'The Hobbit: The Battle of The Five Armies',
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/The_Hobbit_-_The_Battle_of_the_Five_Armies.png/220px-The_Hobbit_-_The_Battle_of_the_Five_Armies.png',
       description:
         "Middle-earth is thrown into a spectacular war as the Smaug's wrath unleashes chaos. Heroes and races clash for control of the Lonely Mountain, leading to a climactic battle that will determine the fate of the realm. Loyalties are tested, sacrifices made, and destinies fulfilled in this epic conclusion to Bilbo Baggins's extraordinary journey",
       genre:
