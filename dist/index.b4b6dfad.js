0fd54f9adbab5eb60864d130cc1fcdc9cb26e2cd
bradkcrist <bradkcrist08@gmail.com>
2024-03-21 04:37:29 -0600

    Trying to test why my api can't be fetched from my  movie_api

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27330,52 +27330,33 @@ var _movieCard = require("../movie-card/movie-card");
 var _s = $RefreshSig$();
 const MainView = ()=>{
     _s();
-    const [movies, setMovies] = (0, _react.useState)([
-        {
-            id: 1,
-            title: "The Hobbit: An Unexpected Journey",
-            image: "https://upload.wikimedia.org/wikipedia/en/b/b3/The_Hobbit-_An_Unexpected_Journey.jpeg",
-            description: "A reluctant hobbit named Bilbo Baggins is swept into a quest by the wizard Gandalf. Joining a company of dwarves led by Thorin Oakenshield, Bilbo embarks on a perilous journey to reclaim the Lonely Mountain and its treasure from the fearsome dragon Smaug.",
-            genre: "Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.",
-            director: "Peter Jackson",
-            bio: "New Zealander director and screenwriter",
-            birth: "10-31-1961"
-        },
-        {
-            id: 2,
-            title: "The Hobbit: The Desolation of Smaug",
-            image: "https://upload.wikimedia.org/wikipedia/en/4/4f/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg",
-            description: "Bilbo Baggins navigates perilous landscapes and encounters dangerous creatures as he joins a quest to reclaim treasure guarded by the dragon Smaug. Unraveling secrets and testing alliances, the journey becomes a thrilling adventure where friendships are forged and courage is tested in the heart of Middle-earth.",
-            genre: "Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.",
-            director: "Peter Jackson",
-            bio: "New Zealander director and screenwriter",
-            birth: "10-31-1961"
-        },
-        {
-            id: 3,
-            title: "The Hobbit: The Battle of The Five Armies",
-            image: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/The_Hobbit_-_The_Battle_of_the_Five_Armies.png/220px-The_Hobbit_-_The_Battle_of_the_Five_Armies.png",
-            description: "Middle-earth is thrown into a spectacular war as the Smaug's wrath unleashes chaos. Heroes and races clash for control of the Lonely Mountain, leading to a climactic battle that will determine the fate of the realm. Loyalties are tested, sacrifices made, and destinies fulfilled in this epic conclusion to Bilbo Baggins's extraordinary journey",
-            genre: "Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.",
-            director: "Peter Jackson",
-            bio: "New Zealander director and screenwriter",
-            birth: "10-31-1961"
-        }
-    ]);
+    const [movies, setMovies] = (0, _react.useState)([]);
+    (0, _react.useEffect)(()=>{
+        fetch("https://mymovie1-195f3788c76b.herokuapp.com/").then((response)=>response.json()).then((data)=>{
+            const moviesFromApi = data.docs.map((doc)=>{
+                return {
+                    id: doc.key,
+                    title: doc.title,
+                    director: doc.director_name?.[0]
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    }, []);
     const [selectedMovie, setSelectedMovie] = (0, _react.useState)(null);
     if (selectedMovie) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieView.MovieView), {
         movie: selectedMovie,
         onBackClick: ()=>setSelectedMovie(null)
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 48,
+        lineNumber: 27,
         columnNumber: 12
     }, undefined);
     if (movies.length === 0) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         children: "The list is empty"
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 52,
+        lineNumber: 31,
         columnNumber: 12
     }, undefined);
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -27387,17 +27368,17 @@ const MainView = ()=>{
                 }
             }, movie.id, false, {
                 fileName: "src/components/main-view/main-view.jsx",
-                lineNumber: 59,
+                lineNumber: 38,
                 columnNumber: 11
             }, undefined);
         })
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 56,
+        lineNumber: 35,
         columnNumber: 5
     }, undefined);
 };
-_s(MainView, "thccsY5IBUGQ8XX5xHPRX4CCS7w=");
+_s(MainView, "llzgrUkvR/+OoCNfiqlA1H2LLFI=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,46 +1,25 @@
 import { useState } from 'react';
 import { MovieView } from '../movie-view/movie-view';
 import { MovieCard } from '../movie-card/movie-card';
+import { useState, useEffect } from 'react';
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: 'The Hobbit: An Unexpected Journey',
-      image: 'https://upload.wikimedia.org/wikipedia/en/b/b3/The_Hobbit-_An_Unexpected_Journey.jpeg',
-      description:
-        'A reluctant hobbit named Bilbo Baggins is swept into a quest by the wizard Gandalf. Joining a company of dwarves led by Thorin Oakenshield, Bilbo embarks on a perilous journey to reclaim the Lonely Mountain and its treasure from the fearsome dragon Smaug.',
-      genre:
-        'Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.',
-      director: 'Peter Jackson',
-      bio: 'New Zealander director and screenwriter',
-      birth: '10-31-1961',
-    },
-    {
-      id: 2,
-      title: 'The Hobbit: The Desolation of Smaug',
-      image: 'https://upload.wikimedia.org/wikipedia/en/4/4f/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg',
-      description:
-        'Bilbo Baggins navigates perilous landscapes and encounters dangerous creatures as he joins a quest to reclaim treasure guarded by the dragon Smaug. Unraveling secrets and testing alliances, the journey becomes a thrilling adventure where friendships are forged and courage is tested in the heart of Middle-earth.',
-      genre:
-        'Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.',
-      director: 'Peter Jackson',
-      bio: 'New Zealander director and screenwriter',
-      birth: '10-31-1961',
-    },
-    {
-      id: 3,
-      title: 'The Hobbit: The Battle of The Five Armies',
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/The_Hobbit_-_The_Battle_of_the_Five_Armies.png/220px-The_Hobbit_-_The_Battle_of_the_Five_Armies.png',
-      description:
-        "Middle-earth is thrown into a spectacular war as the Smaug's wrath unleashes chaos. Heroes and races clash for control of the Lonely Mountain, leading to a climactic battle that will determine the fate of the realm. Loyalties are tested, sacrifices made, and destinies fulfilled in this epic conclusion to Bilbo Baggins's extraordinary journey",
-      genre:
-        'Fantasy, Fantasy movie is a film genre that showcases imaginative and otherworldly elements, often featuring magical realms, mythical creatures, and extraordinary events that transcend the boundaries of reality.',
-      director: 'Peter Jackson',
-      bio: 'New Zealander director and screenwriter',
-      birth: '10-31-1961',
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch('https://mymovie1-195f3788c76b.herokuapp.com/')
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            director: doc.director_name?.[0],
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
